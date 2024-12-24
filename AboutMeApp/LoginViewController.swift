9f3c896a1c82e8f54ecdd87350c3d48879abca33
Hadro0n <v.w.yakunin@gmail.com>
2024-12-24 12:17:24 +0300

    Create AboutMeApp

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var UserNameTextField: UITextField!
+    @IBOutlet var PasswordTextField: UITextField!
+    
+    private let user = "Vladislav"
+    private let password = "123456"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.userName = user
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
-    */
-
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard UserNameTextField.text == user, PasswordTextField.text == password else {
+            showAlert(withTitle: "Неверный логин или пароль", andMessage: "Введите верный логин или пароль") {
+                self.UserNameTextField.text = ""
+                self.PasswordTextField.text = ""
+            }
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func forgotUserNameButtonTapped() {
+        showAlert(withTitle: "Упс!", andMessage: "Ваше имя: \(user)")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        showAlert(withTitle: "Упс!", andMessage: "Ваш пароль: \(password)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        UserNameTextField.text = ""
+        PasswordTextField.text = ""
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName: String!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.setupGradientView()
+        welcomeLabel.text = "Welcome, \(userName ?? "")."
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UIView {
+    func setupGradientView() {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.systemRed.cgColor, UIColor.systemBlue.cgColor]
+        gradient.locations = [0, 0.5, 1]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
-    */
-
 }
